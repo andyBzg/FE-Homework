b631886ba5ce3be1d94b23080db19afa2076a7e6
andyBzg <andr.bzg@gmail.com>
2022-12-10 21:52:35 +0100

    Initial commit

--- Homework_08.12/task_2/script.js
+++ Homework_08.12/task_2/script.js
@@ -3,3 +3,19 @@
 console.log('Hello, World!');
 
 // 2. Создать квадратный div и при наведении курсора на него цвет div-а рандомно меняется.
+
+const magicSquare = document.querySelector('#magicSquare');
+
+// Функция, которая генерирует рандомный цвет
+function randomColor(){
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    let rgb = `rgb(${r}, ${g}, ${b})`;
+    return rgb;
+}
+
+// Прослушка на наведение курсора на квадрат и замена background-color
+magicSquare.addEventListener('mouseover', () => {
+    document.getElementById('magicSquare').style.backgroundColor = randomColor();
+})
